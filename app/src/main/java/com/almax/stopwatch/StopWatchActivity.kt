a5984777bf68a13999e20468fe82f56158443df4
maxi <arora.anubhav93@gmail.com>
2025-03-16 19:05:59 +0530

    Update classes names

---
+++ app/src/main/java/com/almax/stopwatch/StopWatchActivity.kt
@@ -0,0 +1,11 @@
+package com.almax.stopwatch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StopWatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stop_watch)
+    }
+}
