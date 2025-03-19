3cb372f9d6309543c5ae11bd8655b1bbeff4e6f2
maxi <arora.anubhav93@gmail.com>
2025-03-19 18:51:37 +0530

    Update util method to format time

--- app/src/main/java/com/almax/stopwatch/utils/utils.kt
+++ app/src/main/java/com/almax/stopwatch/utils/utils.kt
@@ -3,9 +3,10 @@ package com.almax.stopwatch.utils
 import java.util.Locale
 
 fun formatTime(time: Long): String {
+    val milliseconds = (time % 1000) / 10
     val seconds = (time / 1000) % 60
     val minutes = time / 1000 / 60
-    val milliseconds = (time % 1000) / 10
-
+    /*val hours = (time / (1000 * 60 * 60)) % 24*/
     return String.format(Locale.getDefault(), "%02d:%02d:%02d", minutes, seconds, milliseconds)
+  /*  return String.format(Locale.getDefault(), "%02d:%02d:%02d:%02d", hours, minutes, seconds, milliseconds)*/
 }
