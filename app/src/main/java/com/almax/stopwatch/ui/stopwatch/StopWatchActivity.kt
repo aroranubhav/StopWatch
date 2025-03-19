11c49a23ae0f3ba98d409428ff6cac245fb2ca8e
maxi <arora.anubhav93@gmail.com>
2025-03-19 18:52:23 +0530

    Update activity to interact with the viewmodel and pass ui actions

--- app/src/main/java/com/almax/stopwatch/ui/stopwatch/StopWatchActivity.kt
+++ app/src/main/java/com/almax/stopwatch/ui/stopwatch/StopWatchActivity.kt
@@ -1,22 +1,31 @@
 package com.almax.stopwatch.ui.stopwatch
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.almax.stopwatch.R
+import com.almax.stopwatch.StopWatchApplication
 import com.almax.stopwatch.databinding.ActivityStopWatchBinding
+import com.almax.stopwatch.di.component.ActivityComponent
+import com.almax.stopwatch.di.component.DaggerActivityComponent
+import com.almax.stopwatch.di.module.ActivityModule
 import com.almax.stopwatch.utils.formatTime
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class StopWatchActivity : AppCompatActivity() {
 
+    private lateinit var component: ActivityComponent
     private lateinit var binding: ActivityStopWatchBinding
-    private val viewModel: StopWatchViewModel by viewModels()
+
+    @Inject
+    lateinit var viewModel: StopWatchViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        injectDependencies()
         super.onCreate(savedInstanceState)
         binding = ActivityStopWatchBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,36 +33,60 @@ class StopWatchActivity : AppCompatActivity() {
     }
 
     private fun setupUi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS), 200
+            )
+        }
+
         binding.apply {
             btnTimer.setOnClickListener {
-                viewModel.updateTimer()
+                updateTimer()
             }
             btnReset.setOnClickListener {
-                viewModel.resetTimer()
+                resetTimer()
             }
         }
-        observeDataAndUpdateUi()
     }
 
-    private fun observeDataAndUpdateUi() {
-        //TODO are two launch blocks needed? test this scenario
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.isRunning.collect { isRunning ->
-                    binding.btnTimer.text = if (isRunning) {
-                        resources.getString(R.string.stop)
-                    } else {
-                        resources.getString(R.string.start)
-                    }
+    private fun updateTimer() {
+        val timerBtnTest = binding.btnTimer.text
+
+        if (timerBtnTest == resources.getString(R.string.start)) {
+            viewModel.startTimer()
+
+            lifecycleScope.launch {
+                binding.btnTimer.text = resources.getString(R.string.stop)
+                viewModel.timer.collect { elapsedTime ->
+                    binding.tvTimer.text = formatTime(elapsedTime)
                 }
             }
-        }
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.timer.collect { time ->
-                    binding.tvTimer.text = formatTime(time)
-                }
-            }
+        } else {
+            stopTimer()
         }
     }
+
+    private fun resetTimer() {
+        viewModel.resetTimer()
+        binding.btnTimer.text = resources.getString(R.string.start)
+        binding.tvTimer.text = "00:00:00"
+    }
+
+    private fun stopTimer() {
+        viewModel.stopTimer()
+        binding.btnTimer.text = resources.getString(R.string.start)
+    }
+
+
+    private fun injectDependencies() {
+        component = DaggerActivityComponent
+            .builder()
+            .applicationComponent((application as StopWatchApplication).component)
+            .activityModule(ActivityModule(this@StopWatchActivity))
+            .build()
+        component.inject(this@StopWatchActivity)
+    }
 }
+
+const val MainActivityTAG = "MainActivityTAG"
